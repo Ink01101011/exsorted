7d89851b72145618a67bfe895ca39906cd2fa2e3
Supakit <supakit.kitj@gmail.com>
2026-04-01 23:44:43 +0700

    build: change configurations

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/sorted/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: false,
+  clean: true,
+  minify: true,
+  treeshake: true,
+  outDir: 'dist',
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.mjs',
+    };
+  },
+});
