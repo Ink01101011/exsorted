78de5d70be9190943e7a6a061395148e0ee51465
Supakit <supakit.kitj@gmail.com>
2026-05-11 18:39:16 +0700

    fix: disable watchman in Jest configuration for consistent test runs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  watchman: false,
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   collectCoverageFrom: ['src/**/*.ts'],
